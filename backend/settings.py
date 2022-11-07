c8fce71f953686fdf12091ce70151e08168bd215
NiiOdartei7 <willevans468@gmail.com>
2022-11-07 09:04:42 +0000

    test

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'django-insecure-csa6rsfqp-j30v$&ej$%7##pw8da3g-m1hvwuj9qf))%fkzjyh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = false
 
 ALLOWED_HOSTS = ['*']
 
